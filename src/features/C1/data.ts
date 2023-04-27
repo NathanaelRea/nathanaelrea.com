eff358a6d57ccab53da39c2a27c3f1e2d7bac354
NathanaelRea <Nathan@NathanaelRea.com>
2023-04-27 07:39:44 -0400

    update transactions

--- src/features/C1/data.ts
+++ src/features/C1/data.ts
@@ -35,6 +35,7 @@ export const defaultData: Purchases[] = [
       { value: 0.015, date: new Date("2023-01-01") },
       { value: 0.015, date: new Date("2023-02-01") },
       { value: 0.015, date: new Date("2023-03-01") },
+      { value: 0.015, date: new Date("2023-04-01") },
     ],
   },
   {
@@ -49,30 +50,51 @@ export const defaultData: Purchases[] = [
       { value: 0.5, date: new Date("2023-03-01") },
       { value: 0.5, date: new Date("2023-03-15") },
       { value: 0.5, date: new Date("2023-04-01") },
+      { value: 0.5, date: new Date("2023-04-15") },
     ],
   },
   {
     symbol: "DOGE",
     name: "dogecoin",
     percentTarget: 0.1,
-    buyHistory: [{ value: 15_000, date: new Date("2023-01-01") }],
+    buyHistory: [
+      { value: 5_000, date: new Date("2023-01-01") },
+      { value: 5_000, date: new Date("2023-02-01") },
+      { value: 5_000, date: new Date("2023-03-01") },
+      { value: 5_000, date: new Date("2023-04-01") },
+    ],
   },
   {
     symbol: "SOL",
     name: "solana",
     percentTarget: 0.1,
-    buyHistory: [{ value: 40, date: new Date("2023-01-01") }],
+    buyHistory: [
+      { value: 10, date: new Date("2023-01-01") },
+      { value: 10, date: new Date("2023-02-01") },
+      { value: 10, date: new Date("2023-03-01") },
+      { value: 10, date: new Date("2023-04-01") },
+    ],
   },
   {
     symbol: "XMR",
     name: "monero",
     percentTarget: 0.1,
-    buyHistory: [{ value: 10, date: new Date("2023-01-01") }],
+    buyHistory: [
+      { value: 3, date: new Date("2023-01-01") },
+      { value: 3, date: new Date("2023-02-01") },
+      { value: 3, date: new Date("2023-03-01") },
+      { value: 3, date: new Date("2023-04-01") },
+    ],
   },
   {
     symbol: "LINK",
     name: "chainlink",
     percentTarget: 0.1,
-    buyHistory: [{ value: 250, date: new Date("2023-01-01") }],
+    buyHistory: [
+      { value: 100, date: new Date("2023-01-01") },
+      { value: 100, date: new Date("2023-02-01") },
+      { value: 100, date: new Date("2023-03-01") },
+      { value: 100, date: new Date("2023-04-01") },
+    ],
   },
 ];
