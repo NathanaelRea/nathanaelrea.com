ff4192c7444de14d687907f8f581cf9f667c0834
NathanaelRea <Nathan@NathanaelRea.com>
2023-08-21 21:19:24 -0400

    move to astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{astro,html,md,mdx,ts,tsx}"],
   theme: {
     extend: {},
   },
