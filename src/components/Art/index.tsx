87a2a51d635d8da8aa40a9bed32019aafcf3740a
Nathanael Rea <Nathan@NathanaelRea.com>
2024-01-21 17:16:21 -0500

    upgrade

--- src/components/Art/index.tsx
+++ src/components/Art/index.tsx
@@ -9,12 +9,12 @@ export default function Art() {
   return (
     <div className="flex flex-col w-screen items-center">
       <div className="w-full lg:w-1/2 gap-10 lg:gap-20 flex flex-col">
-        <img src={margot} alt="margot" />
-        <img src={eun} alt="eun" />
-        <img src={sunshine} alt="sunshine" />
-        <img src={bell} alt="bell" />
-        <img src={muteSmash} alt="mute bros smash" />
-        <img src={unwise} alt="unwise" />
+        <img src={margot.src} alt="margot" />
+        <img src={eun.src} alt="eun" />
+        <img src={sunshine.src} alt="sunshine" />
+        <img src={bell.src} alt="bell" />
+        <img src={muteSmash.src} alt="mute bros smash" />
+        <img src={unwise.src} alt="unwise" />
       </div>
     </div>
   );
