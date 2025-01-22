132ac50314fb60cba7a4bca10480ab145d01048a
Nathanael Rea <Nathan@NathanaelRea.com>
2025-01-22 17:15:44 -0500

    Fix nav current page bug dev vs prod (#11)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import * as motion from "motion/react-client";
 import { navigate } from "astro:transitions/client";
+import { cn } from "../lib/utils";
 
 const tabs = [
   { id: "home", label: "Home", href: "/" },
@@ -13,34 +14,39 @@ interface NavProps {
 }
 const Nav: React.FC<NavProps> = ({ defaultPath }) => {
   const [currentPath, setCurrentPath] = useState(defaultPath);
+  const normPath = currentPath.replace(/\/+$/, "") || "/";
 
   return (
-    <div className="flex gap-4">
-      {tabs.map((tab) => (
-        <button
-          onClick={() => {
-            setCurrentPath(tab.href);
-            navigate(tab.href);
-          }}
-          key={tab.id}
-          className={`${
-            currentPath === tab.href ? "" : "hover:text-cyan-500/50"
-          } relative rounded-full px-4 py-2 text-sm font-bold text-cyan-500 transition`}
-          style={{
-            WebkitTapHighlightColor: "transparent",
-          }}
-        >
-          {currentPath === tab.href && (
-            <motion.div
-              layoutId="nav"
-              className="absolute inset-0 z-10 bg-cyan-500 mix-blend-difference"
-              style={{ borderRadius: 9999 }}
-              transition={{ type: "spring", duration: 1 }}
-            />
-          )}
-          {tab.label}
-        </button>
-      ))}
+    <div className="flex gap-4 py-2">
+      {tabs.map((tab) => {
+        const isCurrentPath = normPath === tab.href;
+        return (
+          <button
+            onClick={() => {
+              setCurrentPath(tab.href);
+              navigate(tab.href);
+            }}
+            key={tab.id}
+            className={cn(
+              !isCurrentPath && "hover:text-cyan-500/50",
+              "relative rounded-full px-4 py-2 text-lg font-bold text-cyan-500 transition",
+            )}
+            style={{
+              WebkitTapHighlightColor: "transparent",
+            }}
+          >
+            {isCurrentPath && (
+              <motion.div
+                layoutId="nav"
+                className="absolute inset-0 z-10 bg-cyan-500 mix-blend-difference"
+                style={{ borderRadius: 9999 }}
+                transition={{ type: "spring", duration: 1 }}
+              />
+            )}
+            {tab.label}
+          </button>
+        );
+      })}
     </div>
   );
 };
